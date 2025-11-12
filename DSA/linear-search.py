ca477232529330ddfe13e7b39d5a293e56db2cdf
Akash Dudhbhate <dudhbhateakash8805@gmail.com>
2025-11-12 14:42:43 +0530

    problem-1

---
+++ DSA/linear-search.py
@@ -0,0 +1,44 @@
+''' 
+
+Problem 1 — Linear Search (Easy)
+
+Question:
+Write a Python function that checks if a number exists in a list.
+
+👉 Example:
+
+arr = [5, 10, 15, 20, 25]
+target = 15
+
+
+✅ Expected Output:
+
+15 found at index 2
+
+
+❌ If not found:
+
+30 not found
+
+
+Rules:
+
+Use a for loop (no built-in functions like index()).
+
+Use linear search logic only.
+
+Handle both “found” and “not found” cases.
+
+'''
+
+arr = [5, 10, 15, 20, 25]
+target = 30
+
+for i,val in enumerate(arr):
+    if val == target:
+        print(f"{target} found at index {i}")
+        break
+    elif i == len(arr) - 1:
+        print(f"{target} not found")
+
+    
